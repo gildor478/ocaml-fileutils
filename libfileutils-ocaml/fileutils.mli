2cd65ebb43147ff3723da9610208e86a1862ed74
Sylvain Le Gall <sylvain@le-gall.net>
2003-03-04 07:49:58 +0100

    Initial revision
    Ignore-this: e874781ce098f8372f511e4b9bd218fc
    SVN revision 66
    
    darcs-hash:20030304064958-9e32d-82f7848c585a1625262a9621717290f51468973f

---
+++ libfileutils-ocaml/fileutils.mli
@@ -0,0 +1,60 @@
+(** You cannot pass a base path which is relative *)
+exception Base_path_relative
+(** One of the path you have passed is relative and cannot be reduce *)
+exception Path_relative_unreducable
+
+(** All operation as defined in module Filename *)
+val current_dir_name : string
+val parent_dir_name : string
+val concat : string -> string -> string
+val is_relative : string -> bool
+val is_implicit : string -> bool
+val check_suffix : string -> string -> bool
+val chop_suffix : string -> string -> string
+val chop_extension : string -> string
+val basename : string -> string
+val dirname : string -> string
+val temp_file : string -> string -> string
+val open_temp_file : ?mode:Pervasives.open_flag list -> 
+	string -> string -> string * Pervasives.out_channel
+val quote : string -> string
+
+
+(** Take a list of path component and return the string 
+corresponding to this path *)
+val implode : string list -> string
+
+(** Concat two path component *)
+val concat : string -> string -> string
+
+(** Take a string corresponding to a path a return the component 
+of this path *)
+val explode : string -> string list
+
+(** Remove all path component which are relative inside the path
+For example : /a/../b -> /b/. Path must not be relative *)
+val reduce : string -> string
+
+(** Create an absolute path from a path relative to the base path *)
+val make_absolute : string -> string -> string
+
+(** Create a path which is relative to the base path *)
+val make_relative : string -> string -> string
+
+(** Create a directory listing of the dir *)
+val list_dir : string -> string list
+
+(** Pattern you can use to filter you directory listing *)
+type test_file =
+    Is_file
+  | Is_dir
+  | Is_link
+  | And of test_file * test_file
+  | Or of test_file * test_file
+  | Not of test_file
+
+(** Apply a filtering pattern to a directory listing *)
+val filter_dir : test_file -> string list -> string list
+
+(** Test the existence of the file... *)
+val test : test_file -> string -> bool
