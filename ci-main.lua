da4f47b3571e06a903471ee439b6d9278c709302
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2012-06-12 22:10:49 +0200

    Add continuous integration script
    Ignore-this: 9028d49ca855069e554ebc22572eb659
    
    darcs-hash:20120612201049-4d692-87de8e8260e14e89757fbeedcbccacd18554bac0

---
+++ ci-main.lua
@@ -0,0 +1,25 @@
+
+bootstrap = require("bootstrap")
+
+bootstrap.init()
+
+oasis = require("oasis")
+darcs = require("darcs")
+ci = require("ci")
+godi = require("godi")
+
+ci.init()
+godi.init()
+oasis.init()
+darcs.init()
+
+godi.bootstrap("3.12")
+godi.update()
+godi.upgrade()
+godi.build("godi-findlib")
+godi.build("godi-ounit")
+
+ci.exec("ocaml", "setup.ml", "-configure")
+ci.exec("ocaml", "setup.ml", "-build")
+ci.exec("ocaml", "setup.ml", "-test")
+darcs.create_tag(oasis.package_version())
